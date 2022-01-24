a3c389ea6e434390d43508e26de95acc4f08763c
AndreaAguado <andrea95am@gmail.com>
2022-01-24 18:31:57 +0100

    create types file

---
+++ src/components/types.ts
@@ -0,0 +1,12 @@
+// literal types for the status of each letter box
+
+export type BoxStatus = "absent" | "present" | "correct" | "empty" | "edit";
+
+//type for the status of the game
+
+export const enum GameStatus {
+    Playing,
+    Won,
+    Cost
+}
+
