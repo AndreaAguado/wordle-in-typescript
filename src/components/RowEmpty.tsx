faeea5bcc4281ca413870d3401381f1fbf1d6a70
AndreaAguado <andrea95am@gmail.com>
2022-01-24 20:17:57 +0100

    create and use RowEmpty component

---
+++ src/components/RowEmpty.tsx
@@ -0,0 +1,15 @@
+import Box from "./Box";
+import styles from "../styles/row.module.scss";
+
+const RowEmpty = () => {
+    return(
+       <div className={styles.row}>
+           {Array.from(Array(5)).map((_,i)=>{
+               return(
+                   <Box value="" status="empty"></Box>
+               ) 
+           })}
+       </div> 
+    )
+}
+export default RowEmpty;
--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,9 +1,15 @@
 import RowCompleted from "./RowCompleted";
+import RowEmpty from "./RowEmpty";
 
 const Wordle = () => {
     return(
         <div>
             <RowCompleted word="sabio" solution="break"/>
+            <RowCompleted word="sabio" solution="break"/>
+            <RowCompleted word="sabio" solution="break"/>
+            <RowEmpty/>
+            <RowEmpty/>
+            <RowEmpty/>
         </div>
     )
 }
