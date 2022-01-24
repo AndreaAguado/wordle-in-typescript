3a0b2b0549f0f6267bfbef29b6c231be4c89a7c4
AndreaAguado <andrea95am@gmail.com>
2022-01-24 22:32:51 +0100

    designate solution with useEffect and initialize all the states

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import RowCompleted from "./RowCompleted";
 import RowEmpty from "./RowEmpty";
 import RowCurrent from './RowCurrent';
+import { GameStatus } from "./types";
 
 const Wordle = () => {
+
+    const [wordOfTheDay, setWordOfTheDay] = useState<string>("");
+    const [turn, setTurn] = useState<number>(1);
+    const [currentWord, setCurrentWord] = useState<string>("");
+    const [completedWord, setCompletedWord] = useState<string>("");
+    const [completedWords, setCompletedWords] = useState<string[]>([]);
+    const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Playing);
+
+    useEffect(()=>{
+        setWordOfTheDay('break');
+    },[]);
+
     return(
         <div>
-            <RowCompleted word="sabio" solution="break"/>
-            <RowCompleted word="sabio" solution="break"/>
+            <RowCompleted word="sabio" solution={wordOfTheDay}/>
+            <RowCompleted word="sabio" solution={wordOfTheDay}/>
             <RowCurrent word="sab"/>
             <RowEmpty/>
             <RowEmpty/>
