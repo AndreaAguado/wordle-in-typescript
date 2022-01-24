5a495e8f96ba53638c4f48f533a4a26c3f242454
AndreaAguado <andrea95am@gmail.com>
2022-01-24 21:50:01 +0100

    create and use component for the current used row, style it and apply it

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,12 +1,13 @@
 import RowCompleted from "./RowCompleted";
 import RowEmpty from "./RowEmpty";
+import RowCurrent from './RowCurrent';
 
 const Wordle = () => {
     return(
         <div>
             <RowCompleted word="sabio" solution="break"/>
             <RowCompleted word="sabio" solution="break"/>
-            <RowCompleted word="sabio" solution="break"/>
+            <RowCurrent word="sab"/>
             <RowEmpty/>
             <RowEmpty/>
             <RowEmpty/>
