f3b8cee023f3ed2f40c581823b592bbf584d6764
AndreaAguado <andrea95am@gmail.com>
2022-01-24 19:31:18 +0100

    create interface for each completed row of the game

---
+++ src/components/rowCompleted.tsx
@@ -0,0 +1,15 @@
+import Box from "./Box";
+
+interface rowCompletedProps {
+    word: string;
+    solution: string;
+
+}
+const rowCompleted = ({word, solution} : rowCompletedProps) => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default rowCompleted;
