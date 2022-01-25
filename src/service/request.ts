35afcadc666c21c2f626212f86ef3d868f66f6b8
AndreaAguado <andrea95am@gmail.com>
2022-01-25 21:15:35 +0100

    create word delivery and validation service

---
+++ src/service/request.ts
@@ -0,0 +1,27 @@
+import { WORDS } from "./words";
+
+const getWords = () => {
+    return WORDS;
+}
+
+export const getWordOfTheDay = () => {
+    const words = getWords();
+    const wordOfTheDay = words[getDayOfTheYear()];
+    return wordOfTheDay.toUpperCase();
+}
+
+export const isValidWord = (word: string) => {
+    const words = getWords();
+    return words.includes(word.toUpperCase());
+}
+
+const getDayOfTheYear = () => {
+    const now = new Date();
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff =
+      (now as any) -
+      (start as any) +
+      (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
+    const oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay);
+}
