7bb049021d75e2384667cb719906ebbe2fc2d511
AndreaAguado <andrea95am@gmail.com>
2022-01-25 13:53:09 +0100

    add logic to write letters of a word

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -3,6 +3,7 @@ import RowCompleted from "./RowCompleted";
 import RowEmpty from "./RowEmpty";
 import RowCurrent from './RowCurrent';
 import { GameStatus } from "./types";
+import useWindow from '../hooks/useWindow';
 
 const Wordle = () => {
 
@@ -13,15 +14,70 @@ const Wordle = () => {
     const [completedWords, setCompletedWords] = useState<string[]>([]);
     const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Playing);
 
+    const keys = [
+        "Q",
+        "W",
+        "E",
+        "R",
+        "T",
+        "Y",
+        "U",
+        "I",
+        "O",
+        "P",
+        "A",
+        "S",
+        "D",
+        "F",
+        "G",
+        "H",
+        "J",
+        "K",
+        "L",
+        "Ñ",
+        "Z",
+        "X",
+        "C",
+        "V",
+        "B",
+        "N",
+        "M",
+      ];
+    
     useEffect(()=>{
         setWordOfTheDay('break');
     },[]);
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        const letter = event.key.toUpperCase();
+        if(event.key === "Backspace" && currentWord.length > 0){
+            return;
+        }
+        if(event.key === "Enter"){
+            return;
+        }
+        if(currentWord.length >= 5) {
+            return;
+        }
+        if(keys.includes(letter)){
+            onInput(letter);
+            return;
+        }
+    }
+
+    const onInput = (letter: string) => {
+        const newWord = currentWord + letter;
+        setCurrentWord(newWord);
+
+    }
+
+    useWindow("keydown", handleKeyDown);
 
     return(
         <div>
             <RowCompleted word="sabio" solution={wordOfTheDay}/>
             <RowCompleted word="sabio" solution={wordOfTheDay}/>
-            <RowCurrent word="sab"/>
+            <RowCurrent word={currentWord}/>
             <RowEmpty/>
             <RowEmpty/>
             <RowEmpty/>
