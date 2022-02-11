0beec8098ced790eeeabce33c04e5e9f26204bf6
AndreaAguado <andrea95am@gmail.com>
2022-02-11 22:35:15 +0100

    add gear button in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
-import styles from '../styles/header.module.scss';
+import styles from "../styles/header.module.scss";
 
 const Header = () => {
-    return(
-        <header className={styles.header}>
-            <h1 className={styles.header_title}>Wordle</h1>
-        </header>
-    )
-}
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.header_title}>Wordle</h1>
+      <div>
+        <button className={styles.options}>
+          <i className="bi bi-gear-fill"></i>
+        </button>
+      </div>
+    </header>
+  );
+};
 export default Header;
