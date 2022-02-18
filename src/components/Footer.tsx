afdd2932f03b260a311f97dd04d37f47c637ef98
AndreaAguado <andrea95am@gmail.com>
2022-02-18 17:22:28 +0100

    change footer styles to fit selected theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,53 @@
+import "../styles/footer.scss";
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <small className="footer_small">Made with <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2021</small>
-            <nav className="footer_nav">
-                <ul className="footer_links_list">
-                    <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className='bx bxl-twitter' ></i></a></li>
-                    <li><a className="footer_link" href="https://www.linkedin.com/in/andrea-aguado-moleon/" target="_blank" rel="noreferrer" title="linkedin.com/in/andrea-aguado-moleón/"><i className='bx bxl-linkedin'></i></a></li>
-                    <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i className='bx bxl-github' ></i></a></li>
-                </ul>
-            </nav>
-
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <small className="footer_small">
+        Made with{" "}
+        <span role="img" aria-label="emoji corazón" title="emoji corazón">
+          &#10084;&#65039;
+        </span>{" "}
+        &copy; Andrea Aguado 2021
+      </small>
+      <nav className="footer_nav">
+        <ul className="footer_links_list">
+          <li>
+            <a
+              className="footer_link"
+              href="https://twitter.com/andrea114am"
+              target="_blank"
+              rel="noreferrer"
+              title="twitter.com/andrea114am"
+            >
+              <i className="bx bxl-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              className="footer_link"
+              href="https://www.linkedin.com/in/andrea-aguado-moleon/"
+              target="_blank"
+              rel="noreferrer"
+              title="linkedin.com/in/andrea-aguado-moleón/"
+            >
+              <i className="bx bxl-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              className="footer_link"
+              href="https://github.com/AndreaAguado"
+              target="_blank"
+              rel="noreferrer"
+              title="github.com/AndreaAguado"
+            >
+              <i className="bx bxl-github"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
 
 export default Footer;
